f08ae2c93cfd890f9ed97a94ce085b5458fd4c5f
“Ashutosh-Bhadauriya” <ashutoshb455@gmail.com>
2023-10-31 10:59:40 +0530

    minor fix

--- src/components/HankoAuth.jsx
+++ src/components/HankoAuth.jsx
@@ -1,30 +1,79 @@
-import { useEffect, useCallback, useMemo } from "react";
+// import { useEffect, useCallback, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { register } from "@teamhanko/hanko-elements";
+
+// const hankoApi = process.env.REACT_APP_HANKO_API_URL;
+
+// export default function HankoAuth() {
+//   const navigate = useNavigate();
+//   const [hanko, setHanko] = useState();
+ 
+//   useEffect(() => {
+//     import("@teamhanko/hanko-elements").then(({ Hanko }) =>
+//       setHanko(new Hanko(hankoApi))
+//     );
+//   }, []);
+
+//   const redirectAfterLogin = useCallback(() => {
+//     navigate("/dashboard");
+//   }, [navigate]);
+
+//   useEffect(
+//     () =>
+//       hanko.onAuthFlowCompleted(() => {
+//         redirectAfterLogin();
+//       }),
+//     [hanko, redirectAfterLogin]
+//   );
+
+//   useEffect(() => {
+//     register(hankoApi).catch((error) => {
+//       console.log(error)
+//     });
+//   }, []);
+
+//   return <hanko-auth />;
+// }
+
+
+ 
+import { useEffect, useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { register, Hanko } from "@teamhanko/hanko-elements";
 
+import { register } from "@teamhanko/hanko-elements";
+ 
 const hankoApi = process.env.REACT_APP_HANKO_API_URL;
-
+ 
 export default function HankoAuth() {
   const navigate = useNavigate();
-  const hanko = useMemo(() => new Hanko(hankoApi), []);
 
+ 
+  const [hanko, setHanko] = useState();
+ 
+  useEffect(() => {
+    import("@teamhanko/hanko-elements").then(({ Hanko }) =>
+      setHanko(new Hanko(hankoApi))
+    );
+  }, []);
+ 
   const redirectAfterLogin = useCallback(() => {
+    // successfully logged in, redirect to a page in your application
     navigate("/dashboard");
   }, [navigate]);
-
+ 
   useEffect(
     () =>
-      hanko.onAuthFlowCompleted(() => {
+      hanko?.onAuthFlowCompleted(() => {
         redirectAfterLogin();
       }),
     [hanko, redirectAfterLogin]
   );
-
+ 
   useEffect(() => {
     register(hankoApi).catch((error) => {
-      console.log(error)
+      // handle error
     });
   }, []);
-
+ 
   return <hanko-auth />;
 }
